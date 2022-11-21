44b4dc4b99241f9913ee66812e0fc1872546d687
Matilda Mared <matildamared@live.se>
2022-11-21 13:44:42 +0100

    add tasks repository

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,6 @@
+import { Injectable } from "@nestjs/common";
+import { EntityRepository, Repository } from "typeorm";
+import { Task } from "./dto/task.entity";
+
+@Injectable()
+export class TasksRepository extends Repository<Task> {}
